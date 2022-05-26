a84f465727f285e5f45fa66d6465b8bd944f5bfa
colour-me-bibi <bennyforeman1@gmail.com>
2022-05-26 10:38:25 -0700

    Reset code for pair programming

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,31 +1,4 @@
-/**
- * Returns True if the two objects are shallowly equal, False otherwise.
- * 
- * @param {Object} a The first object to compare
- * @param {Object} b The second object to compare
- * @returns {Boolean} Whether the objects are equal
- */
-const objectEqualsShallow = (a, b) => {
-  const allKeys = new Set([...Object.keys(a), ...Object.keys(b)]);
 
-  return Array.from(allKeys).every((key) => a[key] === b[key]);
-};
-
-/**
- * Returns an Object where the keys are the characters in the string and the values are the number of times that character appears in the string.
- * 
- * @param {String} string The string to count the characters of
- * @returns {Object} An object with the character counts of the string
- */
-const createCounter = (string) => {
-  const counter = {};
-
-  for (let char of string) {
-    counter[char] = (counter[char] || 0) + 1;
-  }
-
-  return counter;
-};
 
 /**
  * Returns whether the strings are case-insensitive anagrams of each other.
@@ -35,22 +8,7 @@ const createCounter = (string) => {
  * @returns
  */
 const isCharacterMatch = (string1, string2) => {
-  const validChars = new Set(
-    Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890")
-  );
 
-  const string1Counts = createCounter(
-    Array.from(string1)
-      .filter((char) => validChars.has(char))
-      .map((char) => char.toLowerCase())
-  );
-  const string2Counts = createCounter(
-    Array.from(string2)
-      .filter((char) => validChars.has(char))
-      .map((char) => char.toLowerCase())
-  );
-
-  return objectEqualsShallow(string1Counts, string2Counts);
 };
 
 module.exports = { isCharacterMatch };
--- python/character_match.py
+++ python/character_match.py
@@ -1,8 +1,5 @@
 # Don't forget to run your tests!
 
-from collections import Counter
-from string import ascii_letters, digits
-
 
 def is_character_match(string1, string2):
     """
@@ -16,9 +13,3 @@ def is_character_match(string1, string2):
 		bool: True if the two strings are anagrams of each other regardless of case.
     """
 
-    valid_chars = set(ascii_letters) | set(digits)
-	
-    string1_counts = Counter(c.lower() for c in string1 if c in valid_chars)
-    string2_counts = Counter(c.lower() for c in string2 if c in valid_chars)
-
-    return string1_counts == string2_counts
